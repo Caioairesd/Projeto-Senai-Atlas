28092a7aa59b43c0277d582db9208eb345695fb5
Caio Vinicius <caioviniaires@gmail.com>
2025-09-02 20:49:46 -0300

    Feat:
    Estrutura Html de cadastro funcionário OK

--- Estoque_jogos/app/views/funcionario/criar_funcionario.php
+++ Estoque_jogos/app/views/funcionario/criar_funcionario.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Cadastrar funcionário</title>
 </head>
 
 <body>
@@ -13,18 +13,24 @@
     <form action="" method="post">
         <label for="nome">Nome:</label>
         <input type="text" id="nome" name="nome">
-        
+
         <label for="cpf">CPF:</label>
         <input type="text" id="cpf" name="cpf">
-
-        <label for="email">Email:</label>
-        <input type="email" id="email" name="email">
 
         <label for="telefone">Telefone:</label>
         <input type="tel" id="telefone" name="telefone">
 
         <label for="endereco">Endereço:</label>
         <input type="text" id="endereco" name="endereco">
+
+        <label for="data_nascimento">Data de nascimento:</label>
+        <input type="date" id="data_nascimento" name="data_nascimento">
+
+        <label for="data_admissao">Data de admissão:</label>
+        <input type="date" id="data_admissao" name="data_admissao">
+
+        <label for="salario">Salário:</label>
+        <input type="text" id="salario" name="salario">
 
         <label for="data_contratacao">Data de contratação:</label>
         <input type="date" id="data_contratacao" name="data_contratacao">
@@ -36,11 +42,17 @@
 
         </select>
 
+        <label for="email">Email:</label>
+        <input type="email" id="email" name="email">
 
+        <label for="usuario">Usuário:</label>
+        <input type="text" id="usuario" name="usuario">
 
+        <label for="senha">senha:</label>
+        <input type="text" id="senha" name="senha">
 
-
-
+        <button type="submit">Cadastrar</button>
+        <button type="reset">Limpar</button>
 
     </form>
 
