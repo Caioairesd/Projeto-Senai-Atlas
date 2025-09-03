c8aecd10ef3aa4f6e30806a24ecc42f373aface1
baleradaniel <danielbalera021@gmail.com>
2025-09-03 01:43:14 -0300

    Triggers e Views
    Foi feita a logica dos triggers e views relacionadas ao estoque

--- Database/atlas_db.sql
+++ Database/atlas_db.sql
@@ -1,81 +1,334 @@
-CREATE DATABASE atlas_db;
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Tempo de geração: 03/09/2025 às 02:38
+-- Versão do servidor: 10.4.32-MariaDB
+-- Versão do PHP: 8.2.12
 
-USE atlas_db;
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
 
-CREATE TABLE fornecedor(
-    id_fornecedor INT AUTO INCREMENT PRIMARY KEY,
-    nome_fornecedor VARCHAR(50) NOT NULL,
-    contato_fornecedor VARCHAR(50),
-    email_fornecedor VARCHAR(50) NOT NULL UNIQUE,
-    cnpj_fornecedor VARCHAR(20) NOT NULL UNIQUE
-);
 
-CREATE TABLE produto(
-    id_produto INT AUTO INCREMENT PRIMARY KEY,
-    nome_produto VARCHAR(50) NOT NULL,
-    descricao_produto TEXT,
-    plataforma_produto VARCHAR(30) NOT NULL,
-    tipo_produto VARCHAR(30) NOT NULL,
-    preco_produto DECIMAL(10, 2) NOT NULL,
-    qtde_estoque_produto INT NOT NULL,
-    imagem_url_produto VARCHAR(255),
-    fornecedor_id INT,
-    FOREIGN KEY (fornecedor_id) REFERENCES fornecedor(id_fornecedor)
-);
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
 
-CREATE TABLE perfil(
-    id_perfil INT AUTO INCREMENT PRIMARY KEY,
-    nome_perfil VARCHAR(30) NOT NULL UNIQUE
-);
+--
+-- Banco de dados: `atlas_db`
+--
 
-CREATE TABLE usuario(
-    id_usuario INT AUTO INCREMENT PRIMARY KEY,
-    nome_usuario VARCHAR(30) NOT NULL,
-    email_usuario VARCHAR(50) NOT NULL UNIQUE,
-    senha_usuario VARCHAR(50) NOT NULL,
-    perfil_id INT,
-    FOREIGN KEY (perfil_id) REFERENCES perfil(id_perfil)
-);
+-- --------------------------------------------------------
 
-CREATE TABLE cliente(
-    id_cliente INT AUTO INCREMENT PRIMARY KEY,
-    nome_cliente VARCHAR(50) NOT NULL,
-    email_cliente VARCHAR(50) NOT NULL UNIQUE,
-    telefone_cliente VARCHAR(20),
-    email_cliente VARCHAR(50),
-    cnpj_fornecedor VARCHAR(20) NOT NULL UNIQUE
-);
+--
+-- Estrutura para tabela `cliente`
+--
 
-CREATE TABLE estoque(
-    id_estoque INT AUTO INCREMENT PRIMARY KEY,
-    produto_id INT,
-    tipo_estoque ENUM('Entrada','Saída') NOT NULL,
-    qtde_estoque INT NOT NULL,
-    data_entrada DATE,
-    data_saida DATE,
-    observacao_estoque TEXT,
-    usuario_id INT,
-    FOREIGN KEY (usuario_id) REFERENCES usuario(id_usuario),
-    FOREIGN KEY (produto_id) REFERENCES produto(id_produto)
-);
+CREATE TABLE `cliente` (
+  `id_cliente` int(11) NOT NULL,
+  `nome_cliente` varchar(50) NOT NULL,
+  `email_cliente` varchar(50) NOT NULL,
+  `telefone_cliente` varchar(20) DEFAULT NULL,
+  `cnpj_fornecedor` varchar(20) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
-CREATE TABLE pedidos(
-    id_pedido INT AUTO INCREMENT PRIMARY KEY,
-    cliente_id INT,
-    data_pedido DATE NOT NULL,
-    status_pedido ENUM('Pendente','Processando','Enviado','Entregue','Cancelado') NOT NULL,
-    valor_total DECIMAL(10, 2) NOT NULL,
-    usuario_id INT,
-    FOREIGN KEY (usuario_id) REFERENCES usuario(id_usuario),
-    FOREIGN KEY (cliente_id) REFERENCES cliente(id_cliente)
-);
+-- --------------------------------------------------------
 
-CREATE TABLE item_pedido(
-    id_item_pedido INT AUTO INCREMENT PRIMARY KEY,
-    pedido_id INT,
-    produto_id INT,
-    qtde_item INT NOT NULL,
-    preco_unitario DECIMAL(10, 2) NOT NULL,
-    FOREIGN KEY (pedido_id) REFERENCES pedidos(id_pedido),
-    FOREIGN KEY (produto_id) REFERENCES produto(id_produto)
-);
+--
+-- Estrutura para tabela `estoque`
+--
+
+CREATE TABLE `estoque` (
+  `id_estoque` int(11) NOT NULL,
+  `produto_id` int(11) DEFAULT NULL,
+  `tipo_estoque` enum('Entrada','Saída') NOT NULL,
+  `qtde_estoque` int(11) NOT NULL,
+  `data_entrada` date DEFAULT NULL,
+  `data_saida` date DEFAULT NULL,
+  `observacao_estoque` text DEFAULT NULL,
+  `usuario_id` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura para tabela `fornecedor`
+--
+
+CREATE TABLE `fornecedor` (
+  `id_fornecedor` int(11) NOT NULL,
+  `nome_fornecedor` varchar(50) NOT NULL,
+  `contato_fornecedor` varchar(50) DEFAULT NULL,
+  `email_fornecedor` varchar(50) NOT NULL,
+  `cnpj_fornecedor` varchar(20) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura para tabela `funcionario`
+--
+
+CREATE TABLE `funcionario` (
+  `id_funcionario` int(11) NOT NULL,
+  `nome_funcionario` varchar(50) NOT NULL,
+  `email_funcionario` varchar(50) NOT NULL,
+  `telefone_funcionario` varchar(20) DEFAULT NULL,
+  `cpf_funcionario` varchar(20) NOT NULL,
+  `salario_funcionario` decimal(10,2) DEFAULT NULL,
+  `endereco_funcionario` varchar(100) DEFAULT NULL,
+  `data_nascimento` date DEFAULT NULL,
+  `data_admissao` date DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura para tabela `item_pedido`
+--
+
+CREATE TABLE `item_pedido` (
+  `id_item_pedido` int(11) NOT NULL,
+  `pedido_id` int(11) DEFAULT NULL,
+  `produto_id` int(11) DEFAULT NULL,
+  `qtde_item` int(11) NOT NULL,
+  `preco_unitario` decimal(10,2) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura para tabela `pedidos`
+--
+
+CREATE TABLE `pedidos` (
+  `id_pedido` int(11) NOT NULL,
+  `cliente_id` int(11) DEFAULT NULL,
+  `data_pedido` date NOT NULL,
+  `status_pedido` enum('Pendente','Processando','Enviado','Entregue','Cancelado') NOT NULL,
+  `valor_total` decimal(10,2) NOT NULL,
+  `usuario_id` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura para tabela `perfil`
+--
+
+CREATE TABLE `perfil` (
+  `id_perfil` int(11) NOT NULL,
+  `nome_perfil` varchar(30) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura para tabela `produto`
+--
+
+CREATE TABLE `produto` (
+  `id_produto` int(11) NOT NULL,
+  `nome_produto` varchar(50) NOT NULL,
+  `descricao_produto` text DEFAULT NULL,
+  `plataforma_produto` varchar(30) NOT NULL,
+  `tipo_produto` varchar(30) NOT NULL,
+  `preco_produto` decimal(10,2) NOT NULL,
+  `qtde_estoque_produto` int(11) NOT NULL,
+  `imagem_url_produto` varchar(255) DEFAULT NULL,
+  `fornecedor_id` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura para tabela `usuario`
+--
+
+CREATE TABLE `usuario` (
+  `id_usuario` int(11) NOT NULL,
+  `nome_usuario` varchar(30) NOT NULL,
+  `email_usuario` varchar(50) NOT NULL,
+  `senha_usuario` varchar(50) NOT NULL,
+  `perfil_id` int(11) DEFAULT NULL,
+  `funcionario_id` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Índices para tabelas despejadas
+--
+
+--
+-- Índices de tabela `cliente`
+--
+ALTER TABLE `cliente`
+  ADD PRIMARY KEY (`id_cliente`),
+  ADD UNIQUE KEY `email_cliente` (`email_cliente`),
+  ADD UNIQUE KEY `cnpj_cliente` (`cnpj_fornecedor`) USING BTREE;
+
+--
+-- Índices de tabela `estoque`
+--
+ALTER TABLE `estoque`
+  ADD PRIMARY KEY (`id_estoque`),
+  ADD KEY `usuario_id` (`usuario_id`),
+  ADD KEY `produto_id` (`produto_id`);
+
+--
+-- Índices de tabela `fornecedor`
+--
+ALTER TABLE `fornecedor`
+  ADD PRIMARY KEY (`id_fornecedor`),
+  ADD UNIQUE KEY `email_fornecedor` (`email_fornecedor`),
+  ADD UNIQUE KEY `cnpj_fornecedor` (`cnpj_fornecedor`);
+
+--
+-- Índices de tabela `funcionario`
+--
+ALTER TABLE `funcionario`
+  ADD PRIMARY KEY (`id_funcionario`),
+  ADD UNIQUE KEY `email_funcionario` (`email_funcionario`),
+  ADD UNIQUE KEY `cpf_funcionario` (`cpf_funcionario`);
+
+--
+-- Índices de tabela `item_pedido`
+--
+ALTER TABLE `item_pedido`
+  ADD PRIMARY KEY (`id_item_pedido`),
+  ADD KEY `pedido_id` (`pedido_id`),
+  ADD KEY `produto_id` (`produto_id`);
+
+--
+-- Índices de tabela `pedidos`
+--
+ALTER TABLE `pedidos`
+  ADD PRIMARY KEY (`id_pedido`),
+  ADD KEY `usuario_id` (`usuario_id`),
+  ADD KEY `cliente_id` (`cliente_id`);
+
+--
+-- Índices de tabela `perfil`
+--
+ALTER TABLE `perfil`
+  ADD PRIMARY KEY (`id_perfil`),
+  ADD UNIQUE KEY `nome_perfil` (`nome_perfil`);
+
+--
+-- Índices de tabela `produto`
+--
+ALTER TABLE `produto`
+  ADD PRIMARY KEY (`id_produto`),
+  ADD KEY `fornecedor_id` (`fornecedor_id`);
+
+--
+-- Índices de tabela `usuario`
+--
+ALTER TABLE `usuario`
+  ADD PRIMARY KEY (`id_usuario`),
+  ADD UNIQUE KEY `email_usuario` (`email_usuario`),
+  ADD KEY `perfil_id` (`perfil_id`),
+  ADD KEY `funcionario_id` (`funcionario_id`);
+
+--
+-- AUTO_INCREMENT para tabelas despejadas
+--
+
+--
+-- AUTO_INCREMENT de tabela `cliente`
+--
+ALTER TABLE `cliente`
+  MODIFY `id_cliente` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de tabela `estoque`
+--
+ALTER TABLE `estoque`
+  MODIFY `id_estoque` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de tabela `fornecedor`
+--
+ALTER TABLE `fornecedor`
+  MODIFY `id_fornecedor` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de tabela `funcionario`
+--
+ALTER TABLE `funcionario`
+  MODIFY `id_funcionario` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de tabela `item_pedido`
+--
+ALTER TABLE `item_pedido`
+  MODIFY `id_item_pedido` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de tabela `pedidos`
+--
+ALTER TABLE `pedidos`
+  MODIFY `id_pedido` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de tabela `perfil`
+--
+ALTER TABLE `perfil`
+  MODIFY `id_perfil` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de tabela `produto`
+--
+ALTER TABLE `produto`
+  MODIFY `id_produto` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de tabela `usuario`
+--
+ALTER TABLE `usuario`
+  MODIFY `id_usuario` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- Restrições para tabelas despejadas
+--
+
+--
+-- Restrições para tabelas `estoque`
+--
+ALTER TABLE `estoque`
+  ADD CONSTRAINT `estoque_ibfk_1` FOREIGN KEY (`usuario_id`) REFERENCES `usuario` (`id_usuario`),
+  ADD CONSTRAINT `estoque_ibfk_2` FOREIGN KEY (`produto_id`) REFERENCES `produto` (`id_produto`);
+
+--
+-- Restrições para tabelas `item_pedido`
+--
+ALTER TABLE `item_pedido`
+  ADD CONSTRAINT `item_pedido_ibfk_1` FOREIGN KEY (`pedido_id`) REFERENCES `pedidos` (`id_pedido`),
+  ADD CONSTRAINT `item_pedido_ibfk_2` FOREIGN KEY (`produto_id`) REFERENCES `produto` (`id_produto`);
+
+--
+-- Restrições para tabelas `pedidos`
+--
+ALTER TABLE `pedidos`
+  ADD CONSTRAINT `pedidos_ibfk_1` FOREIGN KEY (`usuario_id`) REFERENCES `usuario` (`id_usuario`),
+  ADD CONSTRAINT `pedidos_ibfk_2` FOREIGN KEY (`cliente_id`) REFERENCES `cliente` (`id_cliente`);
+
+--
+-- Restrições para tabelas `produto`
+--
+ALTER TABLE `produto`
+  ADD CONSTRAINT `produto_ibfk_1` FOREIGN KEY (`fornecedor_id`) REFERENCES `fornecedor` (`id_fornecedor`);
+
+--
+-- Restrições para tabelas `usuario`
+--
+ALTER TABLE `usuario`
+  ADD CONSTRAINT `usuario_ibfk_1` FOREIGN KEY (`perfil_id`) REFERENCES `perfil` (`id_perfil`),
+  ADD CONSTRAINT `usuario_ibfk_2` FOREIGN KEY (`funcionario_id`) REFERENCES `funcionario` (`id_funcionario`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
